770265217aa03e6e4018b87775a8a2b5a17a3eca
Jason DeVelvis <jason@develvis.com>
2014-02-16 14:00:29 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var sphinx = require('./sphinx');
+
+sphinx.launch(launch_callback,command_callback, error_callback);
+
+function launch_callback() {
+
+}
+
+function command_callback(command) {
+    
+}
+
+function error_callback(e) {
+    
+}
