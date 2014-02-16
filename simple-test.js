ad4a35de46ad4d9a632a28817d0377b216880520
Jason DeVelvis <jason@develvis.com>
2014-02-16 13:59:43 +0000

    Initial Commit

---
+++ simple-test.js
@@ -0,0 +1,23 @@
+var st = require('./simple-thing');
+
+st.init(function() { console.log("Fin"); });
+/*
+st.completePair({
+    "requestID": "100001",
+    "result": {
+        "params": {
+            "algorithm": "sha1",
+            "length": 40,
+            "name": "EHMAcom #13@steward",
+            "issuer": "steward",
+            "step": 30,
+            "base32": "NBJU2TBYGIZXE5TMKN5FCVCFGFLEO42XK5YDMM2GGNQWKYKDIZBGK32HIFRFIQKI",
+            "protocol": "totp"
+        },
+        "success": true,
+        "thingID": "14"
+    },
+    "timestamp": 1392263622
+},"{id:1,name:'ehmacom'}");
+*/
+
